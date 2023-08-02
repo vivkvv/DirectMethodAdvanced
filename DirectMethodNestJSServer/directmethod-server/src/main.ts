c5463cd181507190221e5838705f4a699520f329
Valentyn Vovk <vivkvv@gmail.com>
2023-08-02 21:32:11 +0300

    added console output for deployment checking

--- DirectMethodNestJSServer/directmethod-server/src/main.ts
+++ DirectMethodNestJSServer/directmethod-server/src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import morgan from 'morgan';
 
+console.log('1');
+
 async function bootstrap() {
+  console.log('2');
   const app = await NestFactory.create(AppModule);
+  console.log('3');
   app.use(morgan('dev'));
+  console.log('4');
   app.enableCors({});
+  console.log('5');
   await app.listen(3000);
+  console.log('6');
 }
 bootstrap();
+
+console.log('7');
