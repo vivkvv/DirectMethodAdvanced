b0c2f342b66547ef306ffcb7b243f13bbd37f973
Valentyn Vovk <vivkvv@gmail.com>
2023-08-23 21:53:45 +0300

    attempt to fix redirecting issue

--- DirectMethodNestJSServer/directmethod-server/src/auth-redirect-controller/auth-redirect-controller.controller.ts
+++ DirectMethodNestJSServer/directmethod-server/src/auth-redirect-controller/auth-redirect-controller.controller.ts
@@ -3,7 +3,8 @@ import { Controller, Get, Redirect } from '@nestjs/common';
 @Controller()
 export class AuthRedirectControllerController {
   @Get('authorization-checking')
-  @Redirect('https://direct-pqyo.onrender.com/pre-authorization-checking')
+  //   @Redirect('https://direct-pqyo.onrender.com/pre-authorization-checking')
+  @Redirect('/pre-authorization-checking')
   authorizationChecking() {
     // this method is intended to redirection and must not have any logic
     return null;
