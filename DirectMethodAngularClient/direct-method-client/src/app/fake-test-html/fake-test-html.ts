2a58a0777fd7c1ba486f0ee79a2dc91ecb872845
Valentyn Vovk <vivkvv@gmail.com>
2023-08-26 20:25:18 +0300

    added fake component files

---
+++ DirectMethodAngularClient/direct-method-client/src/app/fake-test-html/fake-test-html.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-test',
+    template: `<div>Redirecting...</div>`,
+})
+export class TestComponent implements OnInit {
+    ngOnInit() {
+        // ваша логика перенаправления
+    }
+}
