c2618b291f24e2a066246dbc6d4fe43f4a696677
Valentyn Vovk <vivkvv@gmail.com>
2023-08-31 22:55:00 +0300

    1. source map on server for local debugging in browser 2. new experiments with redirection

--- DirectMethodNestJSServer/directmethod-server/src/auth-redirect-controller/auth-redirect-controller.controller.ts
+++ DirectMethodNestJSServer/directmethod-server/src/auth-redirect-controller/auth-redirect-controller.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Redirect } from '@nestjs/common';
+import { Controller, Get, Res, Redirect } from '@nestjs/common';
+import { Response } from 'express';
 
 const redirect_url =
   process.env.NODE_ENV === 'production'
@@ -8,9 +9,21 @@ const redirect_url =
 @Controller()
 export class AuthRedirectControllerController {
   @Get('authorization-checking')
-  @Redirect(redirect_url)
-  authorizationChecking() {
-    console.log('After Gooogle AuthRedirectControllerController');
-    return null;
+  // @Redirect(redirect_url)
+  // authorizationChecking() {
+  //   console.log('After Gooogle AuthRedirectControllerController');
+  //   return null;
+  // }
+  redirectToAngular(@Res() res: Response) {
+    res.send(`
+    <html>
+      <head><title>Redirecting...</title></head>
+      <body>
+        <script>
+          window.location.href = "/options";
+        </script>
+      </body>
+    </html>
+  `);
   }
 }
