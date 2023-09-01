0383bc5a25baf5349f9db9599792b1c0601308c5
Valentyn Vovk <vivkvv@gmail.com>
2023-09-01 21:26:54 +0300

    understanding redirecting 2

--- DirectMethodNestJSServer/directmethod-server/src/auth-redirect-controller/auth-redirect-controller.controller.ts
+++ DirectMethodNestJSServer/directmethod-server/src/auth-redirect-controller/auth-redirect-controller.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Res, Redirect } from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Get, Redirect, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 const redirect_url =
   process.env.NODE_ENV === 'production'
@@ -10,8 +10,11 @@ const redirect_url =
 export class AuthRedirectControllerController {
   @Get('authorization-checking')
   @Redirect(redirect_url)
-  authorizationChecking() {
+  authorizationChecking(@Req() req: Request) {
     console.log('After Gooogle AuthRedirectControllerController');
+    console.log('Query Parameters:', req.query);
+    console.log('Full url:', req.url);
+    console.log('Hash Fragment:', req.url.split('#')[1]);
     return null;
   }
 }
